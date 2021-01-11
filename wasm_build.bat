f52048cba1bbbc4e0dc156fb1692fb5222ca4e4f
Shaafiee <shaafiee@gmail.com>
2021-01-12 04:28:26 +0800

    cleaning up

--- wasm_build.bat
+++ wasm_build.bat
@@ -1,5 +1,5 @@
 emcc main.cpp -std=c++17 -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s EXPORTED_FUNCTIONS="['_loadJson', '_main']" -s EXTRA_EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap']" -s WASM=1 -s USE_SDL=2 -O1 -s ASSERTIONS=1 -s SAFE_HEAP=1 -fsanitize=undefined -s ALLOW_MEMORY_GROWTH=1 -o player.js 
 
-copy /Y player.js www
-copy /Y player.wasm www
+copy /Y player.js examples\wasm
+copy /Y player.wasm examples\wasm
 
