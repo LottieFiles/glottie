cb55ef88b59fb5d45af1bb3e6500b348740e47df
Shaafiee <shaafiee@gmail.com>
2021-01-12 04:03:11 +0800

    cleaning up

--- player/test.js
+++
@@ -1,8 +0,0 @@
-buffer1 = Module._malloc(someArray.length * someArray.bytes)
-Module.HEAP8.set(someArray, buffer)
-buffer2 = Module._malloc(someOtherArray.length * someOtherArray.bytes)
-Module.HEAP8.set(someOtherArray, buffer2)
-Module.ccall("someCall", null, ["number", "number", "number", "number"],
-		[buffer1, someArray.length, buffer2, someOtherArray.length])
-
-
--- player/test_lots.pl
+++
@@ -1,5 +0,0 @@
-for ($i = 0; $i < 50; $i++) {
-	if (fork()) {
-		$returned = `cat test.json | lottieplayer 1 1 1 1`;
-	}
-}
